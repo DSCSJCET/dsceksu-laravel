2634c8514f0a31faa4d1434b73e2d12b9371e597
DSCSJCET <70589208+DSCSJCET@users.noreply.github.com>
2020-09-03 21:15:55 +0530

    Update seometa.blade.php

--- resources/views/modules/seometa.blade.php
+++ resources/views/modules/seometa.blade.php
@@ -10,14 +10,14 @@
 <link itemprop="url" rel="canonical" href="{{ url()->current() }}" />
 <meta itemprop="name" content="{{ Request::is('/') ? config('app.name') : config('app.nick') }} @yield('title')">
 <meta itemprop="description" content="Developer Student Clubs(DSC) is a Google Developers program for SJCET students to acquire technical skills and solve problems">
-<meta itemprop="image" content="{{ config('app.url') }}/images/favicon_package_v0.16/android-chrome-512x512.png">
+<meta itemprop="image" content="{{ config('app.url') }}images/favicon_package_v0.16/android-chrome-512x512.png">
 
 <!-- Facebook -->
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{ url()->current() }}" />
 <meta property="og:title" content="{{ Request::is('/') ? config('app.name') : config('app.nick') }} @yield('title')" />
 <meta property="og:description" content="Developer Student Clubs(DSC) is a Google Developers program for SJCET students to acquire technical skills and solve problems" />
-<meta property="og:image" content="{{ config('app.url') }}/favicon_package_v0.16/android-chrome-512x512.png" />
+<meta property="og:image" content="{{ config('app.url') }}images/favicon_package_v0.16/android-chrome-512x512.png" />
 <meta property="og:image:type" content="image/png" />
 <meta property="og:image:alt" content="{{ config('app.nick') }}" />
 
@@ -28,5 +28,5 @@
 <meta name="twitter:creator" content="@emmanuelJet_">
 <meta name="twitter:title" content="{{ Request::is('/') ? config('app.name') : config('app.nick') }} @yield('title')">
 <meta name="twitter:description" content="Developer Student Clubs(DSC) is a Google Developers program for SJCET students to acquire technical skills and solve problems">
-<meta name="twitter:image:src" content="{{ config('app.url') }}/images/favicon_package_v0.16/android-chrome-512x512.png">
+<meta name="twitter:image:src" content="{{ config('app.url') }}images/favicon_package_v0.16/android-chrome-512x512.png">
 <meta name="twitter:image:alt" content="{{ config('app.nick') }}">
