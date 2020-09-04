aff24bef53221a7e2bcb0ff1c3d16974f8bbc899
DSCSJCET <70589208+DSCSJCET@users.noreply.github.com>
2020-09-04 12:30:41 +0530

    Update app.blade.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   @include('modules.analytics')
   <!--  @laravelPWA -->
-
+  @laravelPWA
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
